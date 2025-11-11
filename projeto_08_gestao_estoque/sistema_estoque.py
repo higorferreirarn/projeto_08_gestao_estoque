e6576075dea9d8ee5f066ebdb7bb804d9d4266e2
Carlos Emanuel <carlosemanuel2203@gmail.com>
2025-11-11 15:34:33 -0300

    Atualização do código sistema_estoque

--- projeto_08_gestao_estoque/sistema_estoque.py
+++ projeto_08_gestao_estoque/sistema_estoque.py
@@ -44,30 +44,89 @@ movimentacoes = []  # Lista de movimentações
 
 def cadastrar_produto(codigo, nome, categoria, estoque_minimo, preco):
     """Cadastra novo produto."""
-    pass
+    if codigo in produtos:
+        print("Produto já cadastrado!")
+        return
+    
+    produtos[codigo] = {
+        'nome': nome,
+        'categoria': categoria,
+        'estoque_minimo': estoque_minimo,
+        'preco_unitario': preco,
+        'quantidade': 0
+    }
+
+    print(f"Produto '{nome}' cadastrado com sucesso!")
 
 def registrar_movimentacao(produto_codigo, tipo, quantidade, data, motivo):
     """Registra movimentação de estoque."""
-    pass
+    if produto_codigo not in produtos:
+        print("Produto não encontrado!")
+        return
+    
+    if tipo == 'saida' and produtos[produto_codigo]['quantidade']< quantidade:
+       print("Estoque insuficiente para saída!")
+       return
+
+    movimentacoes.append({
+        'produto_codigo': produto_codigo,
+        'tipo': tipo,
+        'quantidade': quantidade,
+        'data': data,
+        'motivo': motivo
+    })
+
+    # Atualiza estoque
+
+    if tipo == 'entrada':
+        produtos[produto_codigo]['quantidade'] += quantidade
+    elif tipo == 'saida':
+        produtos[produto_codigo]['quantidade'] -= quantidade
+
+    print(f"Movimentação '{tipo}' registrada para o produto {produto_codigo}.")
+
 
 def calcular_estoque_atual(codigo):
     """Calcula estoque atual de um produto."""
-    pass
+    if codigo in produtos:
+        p = produtos[codigo]
+        valor_total = p['quantidade'] * p['preco_unitario']
+        status = "OK" if p['quantidade'] >= p['estoque_minimo'] else "EM FALTA"
+        return{
+            'nome': p['nome'],
+            'quantidade': p['quantidade'],
+            'valor_total': valor_total,
+            'status': status
+        }
+    
+    else:
+        print("Produto não encontrado.")
 
 def identificar_produtos_em_falta():
     """Identifica produtos abaixo do estoque mínimo."""
-    pass
+    return [p for p in produtos.values() if p['quantidade'] <p['estoque_minimo']]
 
 def calcular_valor_total_estoque():
     """Calcula valor total do estoque."""
-    pass
+    total = sum(p['quantidade'] * p['preco_unitario'] for p in produtos.values())
+    print(f"Valor total do estoque: R$ {total:.2f}")
+    return total
 
 def gerar_relatorio_inventario():
     """Gera relatório completo de inventário."""
-    pass
+    print("\n RELATÓRIO DE INVENTÁRIO")
+    for codigo, p in produtos.items():
+        info = calcular_estoque_atual(codigo)
+        print(f"{codigo} - {info['nome']} | Qtd: {info['quantidade']} | Valor Total: R$(info['valor_total']:.2f) | Status: {info['status']}")
+    calcular_valor_total_estoque()
 
 def main():
     """Função principal."""
-    pass
+    cadastrar_produto("PROD001", "Notebook Dell", "Informática", 5, 3500.00)
+    registrar_movimentacao("PROD001", "entrada", 10, "2024-01-15", "Compra")
+    registrar_movimentacao("PROD001", "saída", 3, "2024-01-20", "Venda")
+    gerar_relatorio_inventario()
 
 
+if __name__ == "__main__":
+    main()
